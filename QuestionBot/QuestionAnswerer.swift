733a1f865c7002f43130e3130492d402e75d6ce2
Jonathan E. Hardis <jhardis@me.com>
2021-09-28 19:20:37 -0400

    Final result of exercise.

--- QuestionBot/QuestionAnswerer.swift
+++ QuestionBot/QuestionAnswerer.swift
@@ -1,6 +1,31 @@
 struct MyQuestionAnswerer {
     func responseTo(question: String) -> String {
-        // TODO: Write a response
-        return "I'm sorry, I don't understand the question."
+        
+        //TODO: Write a response.
+        
+        let lowerQuestion = question.lowercased()
+        
+        if lowerQuestion == "where are the cookies?" {
+            return "In the cookie jar!"
+        } else if lowerQuestion.hasPrefix("where") {
+            return "To the North!"
+        } else if lowerQuestion.hasPrefix("hello") {
+            return "Why, hello there"
+        } else if lowerQuestion.hasPrefix("who") {
+            return "Who do you think?"
+        } else if lowerQuestion.hasPrefix("why") {
+            return "Why not?"
+        } else if lowerQuestion.hasPrefix("how") {
+            return "Any way that works."
+        } else {
+            
+            let defaultNumber = question.count % 3
+            
+            switch defaultNumber {
+            case 1: return "That really depends"
+            case 2: return "Hmph.  Really??"
+            default: return "Ask me again tomorrow"
+            }
+        }
     }
 }
